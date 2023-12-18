dd50503b37f77825e8a5fc86d8d1690be18742a8
hamadanagisa <kaeno.ana@gmail.com>
2023-12-18 22:14:17 +0900

    pagesフォルダでルーティング設定をする

---
+++ src/pages/posts/firstPost.js
@@ -0,0 +1,9 @@
+export default function firstPost() {
+    return (
+        <div>
+            <h1>最初の段階</h1>
+            <h2>ホームへ戻る</h2>
+        </div>
+    );
+}
+// nf ノーマルファンクションの略でチートシート出てくる
